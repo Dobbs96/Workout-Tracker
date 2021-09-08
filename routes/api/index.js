cbd8bb1417d4bcaaf0c26846d939ad443c3c1ee7
James Dobbs <dobbs282@gmail.com>
2021-09-08 15:15:25 -0400

    routes being made for front end

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,8 +1,29 @@
 const router = require("express").Router();
 const db = require("../../models");
 
-router.get("/", (req, res) => {
-  res.send("API");
+// GET /api/workouts
+router.get("/workouts", (req, res) => {
+  db.Workout.find({})
+    .then((workouts) => {
+      workouts.forEach((workout) => {
+        var total = 0;
+        workout.exercises.forEach((e) => {
+          total += e.duration;
+        });
+        workout.totalDuration = total;
+      });
+
+      res.json(workouts);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
 });
+
+// PUT api/workouts
+
+// POST api/workouts
+
+// GET api/workouts/range
 
 module.exports = router;
