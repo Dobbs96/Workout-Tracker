f7ec3fc708dcce68155d3cca33d54f5962547d1a
James Dobbs <dobbs282@gmail.com>
2021-09-08 15:15:42 -0400

    routes being made for front end

--- routes/api/index.js
+++ routes/api/index.js
@@ -21,9 +21,44 @@ router.get("/workouts", (req, res) => {
 });
 
 // PUT api/workouts
+router.put("/workouts/:id", (req, res) => {
+  db.Workout.findOneAndUpdate(
+    { _id: req.params.id },
+    {
+      $inc: { totalDuration: req.body.duration },
+      $push: { exercises: req.body },
+    },
+    { new: true }
+  )
+    .then((workouts) => {
+      res.json(workouts);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
 
 // POST api/workouts
 
+router.post("/workouts", ({ body }, res) => {
+  db.Workout.create(body)
+    .then((workouts) => {
+      res.json(workouts);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
+
 // GET api/workouts/range
 
+router.get("/workouts/range", (req, res) => {
+  db.Workout.find({})
+    .then((workouts) => {
+      res.json(workouts);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
 module.exports = router;
