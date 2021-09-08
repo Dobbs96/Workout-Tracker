ad050354022abda591b2fe1c381c71451ac389ff
James Dobbs <dobbs282@gmail.com>
2021-09-08 14:56:22 -0400

    added routes folder

--- server.js
+++ server.js
@@ -1,11 +1,8 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
-const path = require("path");
 
 const PORT = process.env.PORT || 3000;
-
-const db = require("./models");
 
 const app = express();
 
@@ -16,18 +13,12 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
+app.use(require("./routes"));
+
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
   useFindAndModify: false,
   useUnifiedTopology: true,
-});
-
-app.get("/stats", (req, res) => {
-  res.sendFile(path.join(`${__dirname}`, "/public", "stats.html"));
-});
-
-app.get("/exercise", (req, res) => {
-  res.sendFile(path.join(`${__dirname}`, "/public", "exercise.html"));
 });
 
 app.listen(PORT, () => {
