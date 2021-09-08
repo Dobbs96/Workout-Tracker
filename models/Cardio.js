9880362ad248ca23e697fda90dfe0aaf12e4a572
James Dobbs <dobbs282@gmail.com>
2021-09-08 09:11:00 -0400

    cardio model done

--- models/Cardio.js
+++ models/Cardio.js
@@ -2,7 +2,23 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const CardioSchema = new Schema({});
+const CardioSchema = new Schema({
+  day: {
+    type: Date,
+    default: Date.now,
+  },
+  exercise: [
+    {
+      type: "cardio",
+      name: { type: string, require: "Please enter a cardio exercise." },
+      distance: { type: Number, require: "Please enter you distance." },
+      duration: {
+        type: Number,
+        require: "Please enter you duration in minutes.",
+      },
+    },
+  ],
+});
 
 const User = mongoose.model("Cardio", CardioSchema);
 
