b9d569c1168eaf584b56c8d952a16915b2e7aaed
James Dobbs <dobbs282@gmail.com>
2021-09-08 09:39:21 -0400

    Workout model done

--- models/Workout.js
+++ models/Workout.js
@@ -2,7 +2,20 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const WorkoutSchema = new Schema({});
+const WorkoutSchema = new Schema({
+  resistance: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Resistance",
+    },
+  ],
+  cardio: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Cardio",
+    },
+  ],
+});
 
 const User = mongoose.model("Workout", WorkoutSchema);
 
